e5afdc266ad494a460358050d9c1ad8630b41e8d
vanduc2514 <vanduc2514@gmail.com>
2022-03-10 23:38:12 +0700

    Add ferris example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+use String;
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans! I'm Duc!!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
+
+    println!("{}", "~".repeat(20)); // Why does this print before ferris_say ?
 }
