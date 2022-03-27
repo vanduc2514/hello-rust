2e2474d71b12c30c04656a4f2cd8d9649c1aba17
vanduc2514 <vanduc2514@gmail.com>
2022-03-27 16:34:09 +0700

    Complete the guessing game

--- chapter-2/guessing_game/src/main.rs
+++ chapter-2/guessing_game/src/main.rs
@@ -1,3 +1,39 @@
+use std::cmp::Ordering;
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("Guess the number!");
+
+        let secret_number = rand::thread_rng().gen_range(1..10);
+
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .ok()
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse()  {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+            // .expect("Please type a number!");
+
+        println!("You Entered: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+        println!("////////////////////////////")
+    }
 }
